9d72338298dad5f082360044925bf83fb56ed861
Rica <40264785+ricaax@users.noreply.github.com>
2025-11-21 23:05:25 -0300

    Add files via upload

---
+++ app/src/vite-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="vite/client" />
+
+interface Window {
+  ethereum?: {
+    isMetaMask?: boolean
+    request: (args: { method: string; params?: any[] }) => Promise<any>
+    send: (method: string, params?: any[]) => Promise<any>
+    on: (event: string, handler: (...args: any[]) => void) => void
+    removeListener: (event: string, handler: (...args: any[]) => void) => void
+  }
+}
+
